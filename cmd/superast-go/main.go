7613725b582d86ca4f2029507efc3cdaeb04c949
Daniel Martí <mvdan@mvdan.cc>
2015-04-14 15:08:11 +0200

    Deduplicate code in json formatting

--- cmd/superast-go/main.go
+++ cmd/superast-go/main.go
@@ -20,6 +20,7 @@ var (
 func main() {
 	flag.Parse()
 
+	var err error
 	fset := token.NewFileSet()
 	f, err := parser.ParseFile(fset, "stdin.go", os.Stdin, 0)
 	if err != nil {
@@ -28,20 +29,18 @@ func main() {
 	a := superast.NewAST(fset)
 	ast.Walk(a, f)
 
+	var b []byte
 	if *pretty {
-		b, err := json.MarshalIndent(a.RootBlock, "", "  ")
-		if err != nil {
-			log.Fatal(err)
-		}
-		if _, err := os.Stdout.Write(b); err != nil {
-			log.Fatal(err)
-		}
-		fmt.Printf("\n")
+		b, err = json.MarshalIndent(a.RootBlock, "", "  ")
 	} else {
-		enc := json.NewEncoder(os.Stdout)
-		if err := enc.Encode(a.RootBlock); err != nil {
-			log.Println(err)
-		}
+		b, err = json.Marshal(a.RootBlock)
 	}
+	if err != nil {
+		log.Fatal(err)
+	}
+	if _, err = os.Stdout.Write(b); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println()
 
 }
