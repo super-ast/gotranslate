1a9c0f7ac4fe54563c2e9011b22f884353adce68
Daniel Martí <mvdan@mvdan.cc>
2015-05-02 12:03:57 +0200

    Split up into visitor and types

--- visitor.go
+++ visitor.go
@@ -13,59 +13,6 @@ var allowedImports = map[string]struct{}{
 	"log": struct{}{},
 }
 
-type id struct {
-	ID int `json:"id"`
-}
-
-type block struct {
-	id
-	Stmts []stmt `json:"statements"`
-}
-
-type dataType struct {
-	id
-	Name string `json:"name"`
-}
-
-type varDecl struct {
-	id
-	Name     string    `json:"name"`
-	DataType *dataType `json:"data-type,omitempty"`
-}
-
-type stmt interface{}
-
-type statement struct {
-	id
-	Line     int        `json:"line"`
-	Type     string     `json:"type"`
-	Name     string     `json:"name,omitempty"`
-	Value    string     `json:"value,omitempty"`
-	DataType *dataType  `json:"data-type,omitempty"`
-	RetType  *dataType  `json:"return-type,omitempty"`
-	Params   []varDecl  `json:"parameters,omitempty"`
-	Args     []stmt     `json:"arguments,omitempty"`
-	Init     *statement `json:"init,omitempty"`
-	Left     *statement `json:"left,omitempty"`
-	Right    *statement `json:"right,omitempty"`
-	Block    *block     `json:"block,omitempty"`
-}
-
-type identifier struct {
-	id
-	Line     int    `json:"line"`
-	Type     string `json:"type"`
-	Value    string `json:"value"`
-}
-
-type structDecl struct {
-	id
-	Line  int       `json:"line"`
-	Type  string    `json:"type"`
-	Name  string    `json:"name"`
-	Attrs []varDecl `json:"attributes"`
-}
-
 type AST struct {
 	curID      int
 	RootBlock  *block
