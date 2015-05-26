636d15a7464bb6503d535f28f245070dafc18420
Daniel Martí <mvdan@mvdan.cc>
2015-05-26 18:56:00 +0200

    Fix building of test infiles

--- tests/special_declarations/in.go
+++ tests/special_declarations/in.go
@@ -7,7 +7,7 @@ func main() {
 	var s string
 	var i1, i2 int
 	var i3, i4 int = 4, 5
-	a, b = 3, "foo"
+	i, s = 3, "foo"
 	c, d := 5, 6
-	fmt.Println(a, b, c, d, i1, i2, i3, i4)
+	fmt.Println(i, s, c, d, i1, i2, i3, i4)
 }
