efb6d3d692bcf132c7959c9a781a96f560db985e
Daniel Martí <mvdan@mvdan.cc>
2015-05-31 00:54:31 +0200

    Improve binary tests

--- tests/assignment/in.go
+++ tests/assignment/in.go
@@ -5,6 +5,6 @@ import "fmt"
 func main() {
 	var i int
 	i = 3
-	i += 2
-	fmt.Println(i)
+	j := 4
+	fmt.Println(i, j)
 }
---
+++ tests/binary/in.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	i := 3 + 4
+	i = 4
+	i += 2
+	fmt.Println(i)
+}
